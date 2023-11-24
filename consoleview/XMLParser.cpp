3e2b5106af90c75dadde3371f29b909f14a8c135
Andrew <zhutavlyov@gmail.com>
2023-11-24 16:23:39 +0300

    рефактор + XMLAdd

--- consoleview/XMLParser.cpp
+++ consoleview/XMLParser.cpp
@@ -6,12 +6,24 @@
 #define USERPASS "password"
 #define USERGROUP "groupId"
 
+#define CONFIG 0
+#define USERS 1
+
 class XMLParser {
 public:
 
 	XMLParser(const std::string& xmlFile) : xmlFile(xmlFile) {
 		xmlInitParser();
 		LIBXML_TEST_VERSION;
+		doc = xmlReadFile(xmlFile.c_str(), "UTF-8", 0);
+		if (doc == nullptr) {
+			throw "Error opening XML";
+		}
+		rootNode = xmlDocGetRootElement(doc);
+		if (rootNode == nullptr) {
+			xmlFreeDoc(doc);
+			throw "NoRootNode";
+		}
 	}
 	~XMLParser() {
 		xmlCleanupParser();
@@ -39,43 +51,20 @@ public:
 	*	Возможное расширение: поиск не только пароля, но и токена для авторизации
 	*/
 	std::string FindPasswordByUserName(std::string *username) {
+		xmlNode* currentNode = rootNode;
 
 		if (*username == "") throw "NoUserName";
 		std::string UsersPass;
-		xmlDocPtr doc = xmlReadFile(xmlFile.c_str(), "UTF-8", 0);
 
-		if (doc == nullptr) {
-			throw "Error opening XML";
-			return "";
-		}
-		xmlNodePtr rootNode = xmlDocGetRootElement(doc);
-		if (rootNode == nullptr) {
-			xmlFreeDoc(doc);
-			throw "NoRootNode";
-			return "";
-		}
+		currentNode = currentNode->children->next;
 
-		rootNode = rootNode->children->next;
-
-		UsersPass = ParseXMLUsers(*username, rootNode);
+		UsersPass = ParseXMLUsers(*username, currentNode);
 
 		xmlFreeDoc(doc);
 		return UsersPass;
 	}
 	/**
-	* Добавление юзеров
-	* @param Username
-	* @param Password
-	* @param Group
-	* 
-	* @returns void
-	* 
-	* Опицонально: добавить уникальный токен
-	*/
-	void AddUser() {
-	}
-	/**
-	* Удаление юзеров
+	* Удаление нод из XML
 	* 
 	* @param (string) Имя юзера, либо его токен
 	* @returns void
@@ -83,25 +72,47 @@ public:
 	void DeleteUser() {
 		
 	}
+	/**
+	* Добавление ноды в хмл
+	* Принимает на вход 5 аргумментов
+	*
+	* @param Integer: тип добавления (0 - в конфиг, 1 - в датабазу)
+	* @param String: Param1 (Config: Header; Users: Username)
+	* @param String: Param2 (Config: Argument; Users: Password)
+	* @param String: Param3 (Config: None; Users: Group)
+	*
+	*/
+	void AddNode(int type, const char* param1, const char* param2, const char* param3) {
+		xmlNode* node = NULL;
+		switch (type) {
+		case CONFIG:
+			node = FindNode(rootNode, "config");
+			if (node == NULL) throw "AddNodeXMLTagNotFound";
+			AddConfigParam(node, param1, param2);
+			break;
+		case USERS:
+			node = FindNode(rootNode, "users");
+			if (node == NULL) throw "AddNodeXMLTagNotFound";
+			AddUser(node, param1, param2, param3);
+			break;
+		default:
+			throw "AddNodeWrongParam";
+			return;
+		}
+	}
 
 private:
 	std::string userpassword;
 	std::string usergroup;
 	std::string* pUserpassword = &userpassword;
 	std::string* pUsergroup = &usergroup;
-
+	xmlDocPtr doc = nullptr;
+	xmlNodePtr rootNode = nullptr;
+	xmlNode* newNode = NULL;
+	xmlNode* childNewNode = NULL;
 	std::string xmlFile;
 
-	void XMLWriteData(const std::string& tagName, const std::string& data) {
-		xmlDocPtr doc = xmlReadFile(xmlFile.c_str(), nullptr, 0);
-		xmlFreeDoc(doc);
-	}
-
-	void XMLDeleteData() {
-		xmlDocPtr doc = xmlReadFile(xmlFile.c_str(), nullptr, 0);
-
-		xmlFreeDoc(doc);
-	}
+	
 	/**
 	* Эта залупа ищет по списку юзеров, пока не найдет совпадение по юзернейму
 	* И если находит совпадение, то передает поинтер на место в файле для метода
@@ -153,7 +164,37 @@ private:
 				//return currentNode->children->content;
 			}
 		}
+	}
 
+	xmlNode* FindNode(xmlNode* node, const char* name) {
+		xmlNode* result;
 
+		if (node == NULL);
+		
+		while (node) {
+			if ((node->type == XML_ELEMENT_NODE) && (xmlStrcmp(node->name, BAD_CAST name) == 0)) {
+				return node;
+			}
+			if (result = FindNode(node->children, name)) return result;
+			node = node->next;
+		}
+		return NULL;
+	}
+	
+	void AddUser(xmlNode* node, const char* username, const char* password, const char* group) {
+		newNode = xmlNewNode(NULL, BAD_CAST "user");
+		childNewNode = xmlNewNode(NULL, BAD_CAST "username");
+		xmlNodeSetContent(childNewNode, BAD_CAST username);
+		xmlAddChild(newNode, childNewNode);
+		childNewNode = xmlNewNode(NULL, BAD_CAST "password");
+		xmlNodeSetContent(childNewNode, BAD_CAST password);
+		xmlAddChild(newNode, childNewNode);
+		childNewNode = xmlNewNode(NULL, BAD_CAST "group");
+		xmlNodeSetContent(childNewNode, BAD_CAST group);
+		xmlAddChild(newNode, childNewNode);
+	}
+
+	void AddConfigParam(xmlNode* node, const char* header, const char* content) {
+		
 	}
 };
