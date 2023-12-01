7e259beb58ad60388d2fd89604c30f93c64e76fe
Andrew <zhutavlyov@gmail.com>
2023-12-01 11:47:16 +0300

    feat(Parser) добавить парсинг конфига
    Добавил метод парсинга конфига в XMLParser, поменял дефайны на constexpr в XMLParser и в Auth

--- consoleview/consoleview.cpp
+++ consoleview/consoleview.cpp
@@ -6,11 +6,6 @@ using namespace std;
 
 int main()
 {
-    /*
-    Auth<string> auth("nigger", "faggot");
-    auth.MakeAuth();
-    auth.CheckUSBDrive();
-    */
     XMLParser asdf("config.xml");
     return 0;
 }
